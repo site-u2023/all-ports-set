2f7ffbb0e6b303a3fddf040b44848a97444f7a21
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-08-19 21:52:02 +0900

    Update dont_mape_setup_rule.sh

--- dont_mape_setup_rule.sh
+++ dont_mape_setup_rule.sh
@@ -12,13 +12,12 @@ nft delete table ip mape_nat
 
 nft add table ip mape_nat
 nft add chain ip mape_nat POSTROUTING { type nat hook postrouting priority 100 \; }
-nft add rule mape_nat POSTROUTING oifname $TUNDEV meta l4proto { tcp, udp, icmp } mark set numgen inc mod $BLOCKS offset 0x11 counter
+nft add rule mape_nat POSTROUTING oifname map-$TUNDEV meta l4proto { tcp, udp, icmp } mark set numgen inc mod $BLOCKS offset 0x11 counter
 
 for r in `seq 1 $BLOCKS ` ; do
     mark=$(( r + 0x10 ))
     port_l=$(( r * PREFIX + PSID * 16 ))
     port_r=$((port_l + 15))
 
-    nft add rule ip mape_nat POSTROUTING oifname $TUNDEV meta l4proto { tcp, udp, icmp } counter mark $mark snat to $IPv4:$port_l-$port_r persistent
+    nft add rule ip mape_nat POSTROUTING oifname map-$TUNDEV meta l4proto { tcp, udp, icmp } counter mark $mark snat to $IP4:$port_l-$port_r persistent
 done
-
