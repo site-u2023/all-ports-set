5b8e14dde5904ed1b4cdd638c2dbf79b9fbf68ef
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-08-23 11:10:45 +0900

    Update selector.sh

--- selector.sh
+++ selector.sh
@@ -56,14 +56,14 @@ function _func4
   reboot
 }
 # OCN Virtual Connect
-function _funca
+function _func_OCN
 while :
 do
-  echo "------------------------------------------------------"
-  echo -e "\033[0;34m[u]: \033[0;39m"Version use MAP.
-  echo -e "\033[0;34m[w]: \033[0;39m"Version without MAP.
-  echo -e "\033[0;31m[r]: \033[0;39m"GO BACK.
-  echo "------------------------------------------------------"
+  echo -e "\033[1;32m ---------------------------------------------------------------\033[0;39m"
+  echo -e "\033[1;32m[u]: \033[0;39m"Version use MAP.
+  echo -e "\033[1;34m[w]: \033[0;39m"Version without MAP.
+  echo -e "\033[1;31m[r]: \033[0;39m"Go Back.
+  echo -e "\033[1;32m ---------------------------------------------------------------\033[0;39m"
   read -p "Please select a key.: " num
   case "${num}" in
     "u" ) _func1 ;;
@@ -72,14 +72,14 @@ do
   esac
 done
 # V6 Plus / IPv6 Option
-function _funcb
+function _func_V6
 while :
 do
-  echo "------------------------------------------------------"
-  echo -e "\033[0;34m[u]: \033[0;39m"Version use MAP.
-  echo -e "\033[0;34m[w]: \033[0;39m"Version without MAP.
-  echo -e "\033[0;31m[r]: \033[0;39m"GO BACK.
-  echo "------------------------------------------------------"
+  echo -e "\033[1;34m ---------------------------------------------------------------\033[0;39m"
+  echo -e "\033[1;32m[u]: \033[0;39m"Version use MAP.
+  echo -e "\033[1;34m[w]: \033[0;39m"Version without MAP.
+  echo -e "\033[1;31m[r]: \033[0;39m"Go Back.
+  echo -e "\033[1;34m ---------------------------------------------------------------\033[0;39m"
   read -p "Please select a key.: " num
   case "${num}" in
     "u" ) _func3 ;;
@@ -90,15 +90,15 @@ done
 # main
 while :
 do
-  echo "------------------------------------------------------"
-  echo -e "\033[0;34m[o]: \033[0;39m"OCN Virtual Connect.
-  echo -e "\033[0;34m[v]: \033[0;39m"V6 Plus / IPv6 Option.
-  echo -e "\033[0;31m[q]: \033[0;39m"EXIT.
-  echo "------------------------------------------------------"
+  echo -e "\033[1;33m Script installation Version -----------------------------------\033[0;39m"
+  echo -e "\033[1;32m[o]: \033[0;39m"OCN Virtual Connect.
+  echo -e "\033[1;34m[v]: \033[0;39m"V6 Plus / IPv6 Option.
+  echo -e "\033[1;31m[q]: \033[0;39m"Exit.
+  echo -e "\033[1;33m ---------------------------------------------------------------\033[0;39m"
   read -p "Please select a key.: " num
   case "${num}" in
-    "o" ) _funca ;;
-    "v" ) _funcb ;;
+    "o" ) _func_OCN ;;
+    "v" ) _func_V6 ;;
     "q" ) exit 0 ;;
   esac
 done
